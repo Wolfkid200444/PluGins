6b9ba6ad4e67dad26d5c0032be782e35d41440cd
Wolfie <Wolfkid200444@gmail.com>
2021-05-26 21:33:28 -0400

    work

--- Roast/Roast.plugin.js
+++ Roast/Roast.plugin.js
@@ -42,7 +42,7 @@ module.exports = (() => {
                 twitter_username: ''
              }
           ],
-          version: '1.0.0',
+          version: '1.0.1',
           description: 'Roast People.\nPlease go over Commands API to change prefix if u wish to',
           github: 'https://github.com/wolfkid200444',
           github_raw: 'https://raw.githubusercontent.com/wolfkid200444/Plugins/master/Roast/Roast.plugin.js'
@@ -57,18 +57,59 @@ module.exports = (() => {
  
           async start() {
              commands.register({
-                command: 'eoast',
+                command: 'roast',
                 description: 'Roast peopke...',
                 usage: '{c} [text to clap on]',
                 executor(args) {
+                    const roasts = [
+                        'My phone battery lasts longer than your relationships.',
+                        'Calm down. Take a deep breath and then hold it for about twenty minutes.',
+                        'Jealousy is a disease. Get well soon, bitch!',
+                        'You’re so real. A real ass.',
+                        'Whoever told you to be yourself gave you really bad advice.',
+                        'If I had a face like yours I’d sue my parents.',
+                        'Where’s your off button?',
+                        'I didn’t change. I grew up. You should try it sometime.',
+                        'I thought I had the flu, but then I realized your face makes me sick to my stomach.',
+                        'The people who know me the least have the most to say.',
+                        'I’m jealous of people who don’t know you.',
+                        'I’m sorry that my brutal honesty inconvenienced your ego.',
+                        'You sound reasonable… Time to up my medication.',
+                        'Aww, it’s so cute when you try to talk about things you don’t understand.',
+                        'Is there an app I can download to make you disappear?',
+                        'I’m sorry, you seem to have mistaken me with a woman who will take your shit.',
+                        'I’m visualizing duck tape over your mouth.',
+                        '90% of your ‘beauty’ could be removed with a Kleenex.',
+                        'I suggest you do a little soul searching. You might just find one.',
+                        'Some people should use a glue stick instead of chapstick.',
+                        'My hair straightener is hotter than you.',
+                        'I have heels higher than your standards.',
+                        'I’d smack you, but that would be animal abuse.',
+                        'Why is it acceptable for you to be an idiot but not for me to point it out?',
+                        'If you’re offended by my opinion, you should hear the ones I keep to myself.',
+                        'If you’re going to be a smart ass, first you have to be smart, otherwise you’re just an ass.',
+                        'Your face is fine but you will have to put a bag over that personality.',
+                        'Hey, I found your nose, it’s in my business again!',
+                        'I’m not an astronomer but I am pretty sure the earth revolves around the sun and not you.',
+                        'I might be crazy, but crazy is better than stupid.',
+                        'It’s scary to think people like you are allowed to vote.',
+                        'Keep rolling your eyes. Maybe you’ll find your brain back there',
+                        'No, no. I am listening. It just takes me a moment to process so much stupid information all at once.',
+                        'I’m sorry, what language are you speaking? It sounds like bullshit.',
+                        'poopoo head',
+                    ];
                     let result
-                    console.log(args)                   
+                    console.log(args)
+                    return {
+                       send: true,
+                       result: roasts[Math.floor(Math.random() * roasts.length)]
+                    }                 
                 }
              });
           };
  
           stop() {
-             commands.unregister('eoast');
+             commands.unregister('roast');
           };
        };
     };
